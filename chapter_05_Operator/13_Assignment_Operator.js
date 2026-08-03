b20c743005393594052c1ac61dcca1b6e52f1187
Suraj <surajkulenka@gmail.com>
2026-08-04 01:21:15 +0530

    Add operator chapter notes and examples

---
+++ chapter_05_Operator/13_Assignment_Operator.js
@@ -0,0 +1,17 @@
+//  Assignment Operators
+
+let x = 10;
+x = "PrrammodDutta";
+console.log(x);
+
+
+let x1 = 10;
+x1 += 5; //  x1 = x1 +5;
+console.log(x1); //15
+
+x1 -= 3; // x1 = x1-3;
+console.log(x1); // 12
+
+x *= 2;        // x = x * 2 → 24
+x /= 4;        // x = x / 4 → 6
+x %= 4; 
---
+++ chapter_05_Operator/15_Comparsion_Operator.js
@@ -0,0 +1,30 @@
+// Compasion Operator (will always result in the boolean, true or false)
+
+// =, ==, ===
+
+
+// = -> Assignment opearator
+// == -> losse comparsion ( sikh vs hindu )
+// === -> strict comparsion ( sikh vs hindu , languge, living)
+
+
+// > , < , >= , <= 
+
+
+console.log(3 > 4);
+console.log(3 < 4);
+console.log(4 >= 4); // (4 > 4 or 4 == 4, or gate)
+
+console.log(5 == "5"); // loose comparsion => value will compare
+console.log(5 === "5"); // strict not allowed - value + datatype both
+
+// === Strict check we will check for both the datatype and value
+// == Lose check we will check either value or data type.
+
+// console.log(3 == "4");
+// ! -> not char (means reverse)
+console.log(5 != "5"); // loose comparison -> either compare datatype or value
+console.log(5 !== "5"); // true
+// console.log(5 !=== "5"); This doesn't exist
+
+console.log(5 === 5);
